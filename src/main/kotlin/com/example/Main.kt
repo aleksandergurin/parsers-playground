eee7abf06e7f0d1d2a0e77c8028fe149b2e177a1
a.gurin <a.gurin@smartweb.com.ua>
2016-05-06 17:30:45 +0300

    Experiments with ANTLR

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -1,6 +1,23 @@
 package com.example
 
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.tree.ParseTreeWalker
+
 
 fun main(args: Array<String>) {
-    println("${SyntaxChecker.parse(System.`in`)}")
+    val lexer = ArithmeticLexer(ANTLRInputStream("x = 2 * (3 + 4)"))
+    val parser = ArithmeticParser(CommonTokenStream(lexer))
+    val walker = ParseTreeWalker()
+
+    walker.walk(
+            object : ArithmeticBaseListener() {
+                override fun enterExpression(ctx: ArithmeticParser.ExpressionContext) {
+                    println("$ctx text: ${ctx.text}")
+                }
+            },
+            parser.equation()
+    )
+
+    //    println("${SyntaxChecker.parse(System.`in`)}")
 }
